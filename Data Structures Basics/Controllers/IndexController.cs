4bf76d069de2fa3c50504faba6fea5c89802289e
Ryan Skalla <rskalla95@gmail.com>
2018-10-01 10:03:34 -0600

    Add Comments

--- Data Structures Basics/Controllers/IndexController.cs
+++ Data Structures Basics/Controllers/IndexController.cs
@@ -8,9 +8,10 @@ namespace Data_Structures_Basics.Controllers
 {
     public class IndexController : Controller
     {
-
+        //Implement Random Function
         public static Random random = new Random();
 
+        //Implement Random Name Function
         public static string randomName()
         {
             string[] names = new string[8] { "Dan Morain", "Emily Bell", "Carol Roche", "Ann Rose", "John Miller", "Greg Anderson", "Arthur McKinney", "Joann Fisher" };
@@ -18,6 +19,7 @@ namespace Data_Structures_Basics.Controllers
             return names[randomIndex];
         }
 
+        //Implement Random Number Function
         public static int randomNumberInRange()
         {
             return Convert.ToInt32(random.NextDouble() * 20);
@@ -26,27 +28,32 @@ namespace Data_Structures_Basics.Controllers
         // GET: Index
         public ActionResult Index()
         { 
-            Queue<string> line = new Queue<string>();
-            Dictionary<string, int> customers = new Dictionary<string, int>();
+            Queue<string> line = new Queue<string>(); //Initialize Queue
+            Dictionary<string, int> customers = new Dictionary<string, int>(); //Initialize Dictionary
 
+            //Add 100 customers to thr Queue
             for (int i = 0; i < 100; i++)
             {
                 line.Enqueue(randomName());
             }
 
+            //Iterate through the queue
             while (line.Count > 0)
             {
+                //If the dictionary already includes the customer, Dequeue and increment by random number
                 if (customers.ContainsKey(line.Peek()))
                 {
                     customers[line.Dequeue()] += randomNumberInRange();
                 }
 
+                //If the dictionary doesn't include the customer, dequeue and add customer to the dictionary with a random number of burgers
                 else
                 {
-                    customers.Add(line.Dequeue(), 0);
+                    customers.Add(line.Dequeue(), randomNumberInRange());
                 }
             }
 
+            //Create table
             ViewBag.Output += "<table class=\"table table-striped table-hover\" style=\"background-color: #fff\">";
             ViewBag.Output += "<thead class=\"thead-dark\">";
             ViewBag.Output += "<tr>";
@@ -56,6 +63,8 @@ namespace Data_Structures_Basics.Controllers
             ViewBag.Output += "</thead>";
             ViewBag.Output += "<tbody id=\"myTable\">";
 
+
+            //Output each customer from the dictionary into the table
             foreach (KeyValuePair<string, int> entry in customers)
             {
                 ViewBag.Output += "<tr>";
